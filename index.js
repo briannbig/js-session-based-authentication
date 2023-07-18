fb66c6b161e1e174c6d5a249d24e75a9a13d8c4e
Brian Barasa <briannbig@gmail.com>
2023-07-18 07:47:29 +0300

    add inline explanation comments

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const app = express();
 
 app.use(
     sessions({
-        secret: 'some secret',
-        cookie: {
+        secret: 'some secret',          // signing the session ID cookie
+        cookie: {                       // object containing the config for session id cookie
             maxAge: 1000 * 60 * 60 * 4,
         },
-        resave: true,
-        saveUninitialized: false
+        resave: true,                   // Forces the session to be saved back to the session store, even if the session data was never modified during the request
+        saveUninitialized: false        // saves a session to the store even if the session was not initiated.
+        // Another important option is store which we can configure to change how/where the session data is stored on the server. By default, this data is stored in the memory, i.e., MemoryStore.
     })
 );
 
